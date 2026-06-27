f082bb2ff66be7bbdabed45b009a944dc694a2d0
PadraicBurns <padraicburns@protonmail.com>
2026-06-27 16:46:40 -0500

    Initial commit
    Create the initial application structure.

---
+++ SpoofOfConcept/SpoofOfConcept/SpoofOfConceptApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpoofOfConceptApp.swift
+//  SpoofOfConcept
+//
+//  Created by Padraic William Burns on 6/27/26.
+//
+
+import SwiftUI
+
+@main
+struct SpoofOfConceptApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SpoofOfConcept/SpoofOfConceptTests/SpoofOfConceptTests.swift
@@ -0,0 +1,18 @@
+//
+//  SpoofOfConceptTests.swift
+//  SpoofOfConceptTests
+//
+//  Created by Padraic William Burns on 6/27/26.
+//
+
+import Testing
+
+struct SpoofOfConceptTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
